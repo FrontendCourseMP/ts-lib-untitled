fe6b650ec3410361ffb4ea6bf0d73dc3b9879534
vorotilin <vorotilin1111@gmail.com>
2025-12-02 10:20:18 +0300

    Воротилин Илья Андреевич: определение типов

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,44 @@
-export type sum = (a: number, b: number) => number
+export type KatyshFormAPI = {
+  form: (formElement: HTMLFormElement) => FormValidator;
+};
+
+export type FormValidator = {
+  field: (fieldName: string) => FieldValidator;
+  validate: () => boolean;
+};
+
+export type FieldValidator = {
+  string: () => StringValidator;
+  number: () => NumberValidator;
+  array: () => ArrayValidator;
+};
+
+export type StringValidator = {
+  min: (message: string) => StringValidator;
+  max: (message: string) => StringValidator;
+  pattern: (pattern: RegExp, message: string) => StringValidator;
+};
+
+export type NumberValidator = {
+  min: (message: string) => NumberValidator;
+  max: (message: string) => NumberValidator;
+};
+
+export type ArrayValidator = {
+  min: (message: string) => ArrayValidator;
+  max: (message: string) => ArrayValidator;
+};
+
+export type ValidityStateType = {
+  valid: boolean;
+  valueMissing: boolean;
+  typeMismatch: boolean;
+  patternMismatch: boolean;
+  tooLong: boolean;
+  tooShort: boolean;
+  rangeUnderflow: boolean;
+  rangeOverflow: boolean;
+  stepMismatch: boolean;
+  badInput: boolean;
+  customError: boolean;
+};
